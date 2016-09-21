51eade1052ed845045f89a76c74351ed76342bca
Atsushi Nakatsugawa <atsushi@moongift.jp>
2016-09-21 22:23:22 +0900

    Init

---
+++ www/js/ar.js
@@ -0,0 +1,21 @@
+// 位置情報の指定
+var location = new AR.RelativeLocation(null, 3, 0, 0);
+    
+// 表示するモデルの指定
+var modelDragon = new AR.Model("assets/dragon_flying.wt3", {
+	scale: {
+	    x: 0.001,
+	    y: 0.001,
+	    z: 0.001
+	},
+	rotate: {
+	    roll: 25,
+	    tilt: 50.0,
+	}
+    });
+    
+var obj = new AR.GeoObject(location, {
+	drawables: {
+	    cam: [modelDragon],
+	}
+    });
